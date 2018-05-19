35b05ce43ad2570dcef7acf830a1ec4728b74d59
Brian Fleming <brianfleming@gmail.com>
2018-05-19 16:05:26 -0400

    try out nav appear on scroll up

--- _source/_vue/app.js
+++ _source/_vue/app.js
@@ -45,5 +45,91 @@ new Vue({
         foot,
         icon,
         trailers
+    },
+    data() {
+        return {
+            navHeight: 120,
+            firstScroll: true,
+            ticking: false,
+            distanceFromTop: window.pageYOffset,
+            lastDistanceFromTop: window.pageYOffset,
+            scrollingUp: null,
+            scrollingDown: null
+        };
+    },
+    created() {
+        window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
+        const self = this;
+
+        setTimeout(function initBindScrolling() {
+            if (self.distanceFromTop === 0) {
+                self.onScroll();
+            }
+
+            window.addEventListener('scroll', function bindScrolling() {
+                if (!self.ticking) {
+
+                    window.requestAnimationFrame(function rafScroll() {
+                        self.onScroll();
+                        self.ticking = false;
+                    });
+
+                    self.ticking = true;
+
+                }
+            });
+        }, 100);
+    },
+    destroyed() {
+        window.removeEventListener('scroll', this.onScroll);
+    },
+    methods: {
+        setDynamicVariables() {
+            this.distanceFromTop = window.pageYOffset;
+            this.screenBottomPos = this.distanceFromTop + window.innerHeight;
+        },
+        onScroll() {
+            this.setDynamicVariables();
+
+            if (this.firstScroll) {
+                this.firstScroll = false;
+                return;
+            }
+
+            if (this.scrollingUp) {
+                this.$el.classList.remove('scrolling-down');
+
+                if (this.distanceFromTop >= this.navHeight * 2) {
+                    this.$el.classList.add('scrolling-up');
+                }
+                else {
+                    if (this.distanceFromTop === 0) {
+                        this.$el.classList.remove('scrolling-up');
+                    }
+                }
+            }
+            else {
+                if (this.distanceFromTop >= this.navHeight) {
+                    this.$el.classList.remove('scrolling-up');
+                    this.$el.classList.add('scrolling-down');
+                }
+            }
+
+            this.handleAffix();
+        },
+        handleAffix() {
+            this.setScrollingDirection();
+            this.lastDistanceFromTop = this.distanceFromTop;
+        },
+        setScrollingDirection() {
+            if (this.distanceFromTop > this.lastDistanceFromTop) {
+                this.scrollingDown = true;
+                this.scrollingUp = false;
+            }
+            else {
+                this.scrollingUp = true;
+                this.scrollingDown = false;
+            }
+        }
     }
 });
